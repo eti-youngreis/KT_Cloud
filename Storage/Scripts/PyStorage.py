cd136f46af46656e6153dcf3b9f4d383b9dd96e1
shana9317 <166340032+shana9317@users.noreply.github.com>
2024-08-06 20:02:21 +0300

    Update PyStorage.py

--- Storage/Scripts/PyStorage.py
+++ Storage/Scripts/PyStorage.py
@@ -9,28 +9,24 @@ class PyStorage:
 
 
     async def get_object_attributes(self,bucket, key, version_id=None,MaxParts=None,PartNumberMarker =None,SSECustomerAlgorithm =None,SSECustomerKey =None,SSECustomerKeyMD5=None,RequestPayer=None,ExpectedBucketOwner =None,ObjectAttributes=None, sync_flag=False):  
-        # Get the metadata for the given key
         if sync_flag:
-            metadata = self.metadata_manager.get_bucket_metadata(bucket,key)   
+            metadata = self.metadata_manager.get_bucket_metadata(bucket, key)
         else:
-            metadata = await asyncio.to_thread(self.metadata_manager.get_bucket_metadata,bucket, key)
+            metadata = await asyncio.to_thread(self.metadata_manager.get_bucket_metadata, bucket, key)
 
-              
         if metadata is None:
             raise FileNotFoundError(f'No metadata found for object {key}')
 
-        # Determine which version to use
+        # Use the latest version if no version ID is specified
         if version_id is None:
-            # If no version is specified, use the latest version
             version_id = max(metadata['versions'].keys(), key=int)
 
-        # Get the metadata for the specified version
+        # Get version metadata
         version_metadata = metadata['versions'].get(str(version_id))
-
         if version_metadata is None:
             raise FileNotFoundError(f'No version found with ID {version_id} for object {key}')
 
-        # Extract the required information for the response
+        # Extract and return object attributes
         attributes = {
             'checksum': version_metadata.get('checksum'),
             'ETag': version_metadata.get('ETag'),
@@ -38,10 +34,11 @@ class PyStorage:
             'ObjectSize': version_metadata.get('ObjectSize'),
             'StorageClass': version_metadata.get('StorageClass', {})
         }
+
+        # Limit ObjectParts to MaxParts if specified
         if MaxParts is not None:
             object_parts = attributes.get('ObjectParts', [])
             if object_parts:
-                # Limit the number of parts returned to MaxParts
                 attributes['ObjectParts'] = object_parts[:MaxParts]
 
         return attributes
@@ -50,22 +47,22 @@ class PyStorage:
 
 
     async def put_object_tagging(self,bucket, key,tags, version_id=None, ContentMD5=None,ChecksumAlgorithm=None,ExpectedBucketOwner=None,RequestPayer=None, sync_flag=True):
-
-        if not isinstance(sync_flag, bool):
-            raise TypeError('async_flag must be a boolean')
         
-        # Update only the TagSet field for a given key and version ID, preserving other fields
+        if not isinstance(sync_flag, bool):
+            raise TypeError('sync_flag must be a boolean')
+
+        # Initialize metadata for the key if not present
         if key not in self.metadata_manager.metadata:
             self.metadata_manager.metadata[key] = {'versions': {}}
 
-        versions = self.metadata_manager.get_versions(bucket,key)
+        versions = self.metadata_manager.get_versions(bucket, key)
         version_id_str = str(version_id)
 
         if version_id_str in versions:
-            # Update the TagSet field if the version exists
+            # Update TagSet for existing version
             versions[version_id_str]['TagSet'] = tags['TagSet']
         elif version_id:
-            # Add a new version with the given data if the version does not exist
+            # Add new version with tags
             versions[version_id_str] = tags
         else:
             latest_version = self.metadata_manager.get_latest_version(key)
@@ -73,8 +70,8 @@ class PyStorage:
                 versions[str(latest_version)]['TagSet'] = tags['TagSet']
             else:
                 versions['0'] = tags
-                
-        # Save metadata either synchronously or asynchronously
+        
+        # Save metadata
         if sync_flag:
             await self.metadata_manager.save_metadata(True)
         else:
@@ -84,17 +81,16 @@ class PyStorage:
 
     
     async def get_object_tagging(self,bucket, key,ExpectedBucketOwner=None,RequestPayer=None, version_id=None,sync_flag=True):
-                # Check if async_flag is a boolean
         if not isinstance(sync_flag, bool):
             raise TypeError('sync_flag must be a boolean')
-        # Retrieve the TagSet for a given key and version ID
-                # Get the metadata for the given key
+
+        # Fetch metadata for the key
         if sync_flag:
-            metadata = self.metadata_manager.get_bucket_metadata(bucket,key)  
+            metadata = self.metadata_manager.get_bucket_metadata(bucket, key)
         else:
-            metadata = await asyncio.to_thread(self.metadata_manager.get_bucket_metadata,bucket, key)
-            
-        versions = self.metadata_manager.get_versions(bucket,key)
+            metadata = await asyncio.to_thread(self.metadata_manager.get_bucket_metadata, bucket, key)
+
+        versions = self.metadata_manager.get_versions(bucket, key)
         if metadata is None or versions is None:
             return []
 
@@ -104,11 +100,9 @@ class PyStorage:
             return versions[version_id_str].get('TagSet', [])
         
         elif not version_id:
-            # If the version ID is not found, try to get the latest version
-                latest_version = self.metadata_manager.get_latest_version(bucket,key)
-                if latest_version:
-                    return versions[str(latest_version)].get('TagSet', [])
-                       
+            # Get TagSet from latest version if version ID is not provided
+            latest_version = self.metadata_manager.get_latest_version(bucket, key)
+            if latest_version:
+                return versions[str(latest_version)].get('TagSet', [])
+        
         return []
-    
-    
