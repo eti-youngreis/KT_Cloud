883bae1d28b7012ac3d25591e77447808fe8ae4f
gili-dev <g0583398@gmail.com>
2024-08-06 16:11:19 +0300

    add tests for StopDBCluster and StartDBCluster

---
+++ DB/Scripts/tests/test_stopDBCluster.py
@@ -0,0 +1,16 @@
+import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Sara_Lea_Marx_Functions import Sara_Lea_Marx_Functions
+
+@pytest.fixture()
+def create_DBCluster(db_setup):
+    db_setup.CreateDBCluster(db_cluster_identifier = "my-cluster1", engine = "postgres", port = 1150, storage_type = "aurora")
+
+def test_can_stop_DBCluster(db_setup, create_DBCluster):
+    assert db_setup.StopDBCluster(db_cluster_identifier = "my-cluster1")
+
+def test_handle_db_cluster_identifier_cannot_be_found(db_setup, create_DBCluster):
+    with pytest.raises(ValueError):
+        db_setup.StopDBCluster(db_cluster_identifier = "invalid-name")
