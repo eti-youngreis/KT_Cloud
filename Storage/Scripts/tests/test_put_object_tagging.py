24f4204c1b379de6b4aceeec89ffdd6801c4805f
shana9317 <166340032+shana9317@users.noreply.github.com>
2024-08-06 19:47:18 +0300

    Update test_put_object_tagging.py

--- Storage/Scripts/tests/test_put_object_tagging.py
+++ Storage/Scripts/tests/test_put_object_tagging.py
@@ -138,93 +138,95 @@ def test_put_object_tagging(create_test_metadata):
     # Test putting object tagging with valid tags and version ID
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = [{'key': '111', 'value': 'Te22st'}]
+    tags = {'TagSet':[{'key': '111', 'value': 'Te22st'}]}
     print(f"Metadata file path: {functions.metadata_manager.metadata }")
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
 
 def test_put_object_tagging_file_without_tags(create_test_metadata):
     # Test putting object tagging with empty tags list and version ID
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = []
+    tags = {'TagSet':[]}
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
 
 def test_put_object_tagging_empty_values_sync(create_test_metadata):
     # Test putting object tagging with empty key and value in tags list
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = [{'key': '', 'value': ''}]
+    tags = {'TagSet':[{'key': '', 'value': ''}]}
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
 
 def test_put_object_tagging_single_key_empty_value(create_test_metadata):
     # Test putting object tagging with a single key and empty value
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = [{'key': 'SingleKey', 'value': ''}]
+    tags = {'TagSet':[{'key': 'SingleKey', 'value': ''}]}
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
 
 def test_put_object_tagging_multiple_keys(create_test_metadata):
     # Test putting object tagging with multiple keys and values
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = [{'key': 'Key1', 'value': 'Value1'}, {'key': 'Key2', 'value': 'Value2'}]
+    tags = {'TagSet':[{'key': 'Key1', 'value': 'Value1'}, {'key': 'Key2', 'value': 'Value2'}]}
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
 
 def test_put_object_tagging_invalid_arguments(create_test_metadata):
     # Test putting object tagging with invalid async_flag argument
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = [{'key': 'Invalid', 'value': 'Arguments'}]
+    tags ={'TagSet': [{'key': 'Invalid', 'value': 'Arguments'}]}
 
     with pytest.raises(TypeError):
-        asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag='a'))
-        asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag='m'))
+        asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag='a'))
+        asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag='m'))
 
 def test_put_object_tagging_empty_file_path(create_test_metadata):
     # Test putting object tagging with an empty file path and valid version ID
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = []
+    tags = {'TagSet':[]}
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
 
 def test_put_object_tagging_large_metadata(create_test_metadata):
     # Test putting object tagging with a large number of tags
     bucket = 'bucket1'
     key = 'object1.txt'
-    large_tags = [{'key': f'Key{i}', 'value': f'Value{i}'} for i in range(1000)]
+    large_tags =[{'key': f'Key{i}', 'value': f'Value{i}'} for i in range(1000)]
+    tags={'TagSet':large_tags}
+    print(tags)
 
-    asyncio.run(functions.put_object_tagging(bucket, key, large_tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, large_tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
     assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == large_tags
@@ -233,10 +235,10 @@ def test_put_object_tagging_special_characters(create_test_metadata):
     # Test putting object tagging with special characters in tags
     bucket = 'bucket1'
     key = 'object1.txt'
-    tags = [{'key': 'Key#1', 'value': 'Value@1'}, {'key': 'Key&2', 'value': 'Value*2'}]
+    tags = {'TagSet':[{'key': 'Key#1', 'value': 'Value@1'}, {'key': 'Key&2', 'value': 'Value*2'}]}
 
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=False))
-    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', async_flag=True))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=False))
+    asyncio.run(functions.put_object_tagging(bucket, key, tags, version_id='2', sync_flag=True))
 
     metadata = functions.metadata_manager.metadata
-    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags
+    assert metadata['server']['buckets'][bucket]['objects'][key]['versions']['2']['TagSet'] == tags["TagSet"]
