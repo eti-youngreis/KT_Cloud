7584ef4e2f28a1f65e85e1a03c1d848878c8d475
saraNoigershel <s3002005505@gmail.com>
2024-08-11 15:14:18 +0300

    add proxy classes

---
+++ DB/Service/Classes/DBProxyEndpointService.py
@@ -0,0 +1,19 @@
+from Abc import DBO
+
+class DBProxyEndpointService(DBO):
+
+    def create(self, *args, **kwargs):
+        """Implement logic to create a DBProxyEndpoint."""
+        pass
+
+    def delete(self, *args, **kwargs):
+        """Implement logic to delete a DBProxyEndpoint."""
+        pass
+
+    def describe(self, *args, **kwargs):
+        """Implement logic to describe a DBProxyEndpoint."""
+        pass
+
+    def modify(self, *args, **kwargs):
+        """Implement logic to modify a DBProxyEndpoint."""
+        pass
---
+++ DB/Service/Classes/DBProxyService.py
@@ -0,0 +1,19 @@
+from Abc import DBO
+
+class DBProxy(DBO):
+
+    def create(self, *args, **kwargs):
+        """Implement logic to create a DBProxy."""
+        pass
+
+    def delete(self, *args, **kwargs):
+        """Implement logic to delete a DBProxy."""
+        pass
+
+    def describe(self, *args, **kwargs):
+        """Implement logic to describe a DBProxy."""
+        pass
+
+    def modify(self, *args, **kwargs):
+        """Implement logic to modify a DBProxy."""
+        pass
