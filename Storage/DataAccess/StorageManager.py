c58de4fdf618dacdc6bda3b0f91e91a0bec6ae34
gili-dev <g0583398@gmail.com>
2024-08-11 14:12:44 +0300

    fixed bug with cache

---
+++ Storage/DataAccess/StorageManager.py
@@ -0,0 +1,18 @@
+
+from typing import Dict, Any
+
+class StorageManager:
+    """here will be storage actions - S3/localFileSystem"""
+    def insert(self) -> None:
+       pass
+
+    def update(self) -> None:
+       pass
+
+    def select(self) -> Dict[str, Any]:
+       pass
+
+    def delete(self) -> None:
+        pass
+
+
