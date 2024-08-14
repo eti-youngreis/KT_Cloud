d65196d0e2958df13d59c8656639ec133dad65b5
Tem-M <temima628@gmail.com>
2024-08-14 12:31:43 +0300

    change filter from class to type

--- DB/Service/Classes/Filter.py
+++ DB/Service/Classes/Filter.py
@@ -1,9 +1,11 @@
-class Filter:
+from typing import List, TypedDict
+
+
+class Filter(TypedDict):
     
     """
     An object passed as a parameter to certain data requests to help filter the results
     The filter name must be one of the names specified in filter_config.json
     """
-    def __init__(self, name, values):
-        self.name = name
-        self.values = values
+    name: str
+    values: List[str]
