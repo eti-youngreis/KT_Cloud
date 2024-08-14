4c5fc0cc40442cd2853cee49faea849d7f055de5
etiyoungreis <etiyoungreis@gmail.com>
2024-08-14 10:33:38 +0300

    Added type annotations and refined Enum usage

--- DB/Models/EventSubscriptionModel.py
+++ DB/Models/EventSubscriptionModel.py
@@ -1,32 +1,41 @@
-from typing import List
+from enum import Enum
+from typing import Dict, List
 from Models.SourceType import SourceType
+
+
+class EventSubscriptionProps(Enum):
+    SUBSCRIPTION_NAME = 'subscription_name'
+    SOURCES = 'sources'
+    EVENT_CATEGORIES = 'event_categories'
+    SNS_TOPIC_ARN = 'sns_topic_arn'
+    SOURCE_TYPE = 'source_type'
 
 
 class EventSubscriptionModel:
 
     def __init__(
         self,
-        subscription_name,
-        sources: List[SourceType, str],
+        subscription_name: str,
+        sources: Dict[SourceType, str],
         event_categories: List[str],
         sns_topic_arn: str,
-        source_type: str
+        source_type: SourceType
     ) -> None:
         self.subscription_name = subscription_name
         self.source_type = source_type
-        self.sources = {source_type.value: {} for source_type in SourceType}
+        self.sources = {source_type.value: set() for source_type in SourceType}
 
-        for source_type, source_id in sources:
-            self.sources[source_type].add(source_id)
+        for source_type, source_id in sources.items():
+            self.sources[source_type.value].add(source_id)
+
         self.event_categories = event_categories
         self.sns_topic_arn = sns_topic_arn
 
     def to_dict(self):
         return {
-            'subscription_name': self.subscription_name,
-            'source_type': self.source_type,
-            'sources': self.sources,
-            'event_categories': self.event_categories,
-            'sns_topic_arn': self.sns_topic_arn
+            EventSubscriptionProps.SUBSCRIPTION_NAME.value: self.subscription_name,
+            EventSubscriptionProps.SOURCE_TYPE.value: self.source_type,
+            EventSubscriptionProps.SOURCES.value: self.sources,
+            EventSubscriptionProps.EVENT_CATEGORIES.value: self.event_categories,
+            EventSubscriptionProps.SNS_TOPIC_ARN.value: self.sns_topic_arn
         }
- 
