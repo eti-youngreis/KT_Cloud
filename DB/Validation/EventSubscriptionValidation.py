ca2a812b1918f5f05ffdbd0b2a0cfcd715ff7591
etiyoungreis <etiyoungreis@gmail.com>
2024-08-14 16:05:11 +0300

    Refactored validation functions to use EventSubscriptionProps enum for consistent parameter keys. Updated validate_sns_topic_arn to check existence in SNS_Topics_ARNs table. Adjusted validation logic to handle kwargs correctly.

--- DB/Validation/EventSubscriptionValidation.py
+++ DB/Validation/EventSubscriptionValidation.py
@@ -2,15 +2,17 @@ from typing import Dict, List
 
 from DB.DataAccess.DataAccessLayer import DataAccessLayer
 from DB.Models.SourceType import SourceType
+from KT_Cloud.DB.Models.EventSubscriptionModel import EventSubscriptionProps
 
 
 subscription_table_name = 'EventSubscription'
 source_table_name = 'Sources'
+sns_topics_arns_table_name = 'SNS_Topics_ARNs'
 
 
 def validate_subscription_props(dal: DataAccessLayer, **kwargs):
 
-    for key, value in kwargs:
+    for key, value in kwargs.items():
 
         if key not in validations:
             raise ValueError('Invalid parameter', key)
@@ -28,7 +30,18 @@ def validate_subscription_name(dal: DataAccessLayer, subscription_name: str, exi
 
 
 def validate_sources(dal: DataAccessLayer, sources: Dict[SourceType, str]):
-    return True
+
+    source_ids = dal.select(source_table_name, '%')
+
+    for source_type, source_id in sources:
+
+        if source_id not in source_ids:
+            raise ValueError('Source id doesn\'t exist', source_id)
+
+        # TODO: Verify how to access 'type' in source_ids
+        if source_ids[source_id]['type'] != source_type:
+            raise ValueError(f"Source ID '{
+                             source_id}' does not match the expected source type '{source_type}'")
 
 
 def validate_event_categories(dal: DataAccessLayer, event_categories: List[str]):
@@ -36,7 +49,11 @@ def validate_event_categories(dal: DataAccessLayer, event_categories: List[str])
 
 
 def validate_sns_topic_arn(dal: DataAccessLayer, sns_topic_arn: str):
-    return True
+
+    sns_topic_arns = dal.select(sns_topics_arns_table_name, '%')
+
+    if sns_topic_arn not in sns_topic_arns:
+        raise ValueError(f"SNS topic ARN '{sns_topic_arn}' doesn't exist.")
 
 
 def validate_source_type(source_type: SourceType):
@@ -44,9 +61,9 @@ def validate_source_type(source_type: SourceType):
 
 
 validations = {
-    'subscription_name': validate_subscription_name,
-    'sources': validate_sources,
-    'event_categories': validate_event_categories,
-    'sns_topic_arn': validate_sns_topic_arn,
-    'source_type': validate_source_type
+    EventSubscriptionProps.SUBSCRIPTION_NAME: validate_subscription_name,
+    EventSubscriptionProps.SOURCES: validate_sources,
+    EventSubscriptionProps.EVENT_CATEGORIES: validate_event_categories,
+    EventSubscriptionProps.SNS_TOPIC_ARN: validate_sns_topic_arn,
+    EventSubscriptionProps.SOURCE_TYPE: validate_source_type
 }
