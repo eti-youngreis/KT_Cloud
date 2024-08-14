a3b136b8793c22bfdd5d284f43bc1d4ed02ec3ae
malkimarxon <m0548501844@gmail.com>
2024-08-14 10:40:57 +0300

    add class objecta

--- Storage/DataAccess/StorageManager.py
+++ Storage/DataAccess/StorageManager.py
@@ -1,18 +1,54 @@
-from DataAccess import ObjectManager
+import os
 from typing import Dict, Any
 
-class StorageManager(ObjectManager):
+class StorageManager:
+    def __init__(self):
+       self.server_path="D:/בוטקמפ/server"
     """here will be storage actions - S3/localFileSystem"""
-    def insert(self) -> None:
-       pass
+    def create(self, bucket, key, version_id, data) -> None:
+      """Creates a new file with the specified content."""
 
-    def update(self) -> None:
-       pass
+      file_path = os.path.join(self.server_path, bucket, key)
 
-    def select(self) -> Dict[str, Any]:
-       pass
+      if key.endswith('/'):
+         # Create a directory
+         os.makedirs(file_path, exist_ok=True)
+         print(f"Directory '{key}' created in bucket '{bucket}'.")
+      else:
+         # Create a file
+         file_name, file_extension = os.path.splitext(key)
+         versioned_file_name = f"{file_name}.v{version_id}{file_extension}"
+         file_path = os.path.join(self.server_path, bucket, versioned_file_name)
+
+         os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
+         with open(file_path, "wb") as f:
+            f.write(data)
+
+         print(f"File '{key}' created in bucket '{bucket}' with version '{version_id}'.")
+
+    def get(self, bucket, key, version_id) -> Dict[str, Any]:
+
+      """Retrieves the content of a specified file in a bucket and version."""
+      file_name, file_extension = os.path.splitext(key)
+      versioned_file_name = f"{file_name}.v{version_id}{file_extension}"
+      file_path = os.path.join(self.server_path, bucket, versioned_file_name)
+
+      if not os.path.exists(file_path):
+         return {"error": "File not found"}
+
+      with open(file_path, 'rb') as f:
+         data = f.read()
+
+      return data
+      # return {"file_name": key, "version": version_id, "content": data}
 
     def delete(self) -> None:
-        pass
+      pass
+    
+    def rename(self)->None:
+      pass
+    
+    def copy(self)->None:
+      pass
 
 
