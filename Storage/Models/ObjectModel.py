9af1662227c1d2a178c290e443db4ebb33ebd009
malkimarxon <m0548501844@gmail.com>
2024-08-14 11:30:58 +0300

    object modeladd

---
+++ Storage/Models/ObjectModel.py
@@ -0,0 +1,12 @@
+from .VesionModel import Versioning
+
+class ObjectModel:
+    def __init__(self, bucket_name, object_key ):
+        self.bucket = bucket_name
+        self.key = object_key
+        # self.tagging = Tagging()
+        self.versioning = Versioning()
+        # self.acl = ACL()
+
+
+  
