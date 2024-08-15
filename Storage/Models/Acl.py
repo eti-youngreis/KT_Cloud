6a5ef242e563e4e53490be80dfb65a5467623f15
shana9317 <166340032+shana9317@users.noreply.github.com>
2024-08-15 09:59:29 +0300

    Update Acl.py

--- Storage/Models/Acl.py
+++ Storage/Models/Acl.py
@@ -7,18 +7,18 @@ class ACL:
         self.permissions.append(permission)
 
     def remove_permission(self, user, permission):
-       """Remove user permission"""
+       # Remove user permission
         if user in self.permissions:
             self.permissions[user].discard(permission)
             if not self.permissions[user]:
                 del self.permissions[user]
 
     def check_permission(self, user, permission):
-        """Check if the user has a certain permission"""
+        # Check if the user has a certain permission
         return permission in self.permissions.get(user, set())
 
     def is_owner(self, user):
-        """Check if the user owns the file"""
+        # Check if the user owns the file
         return self.owner == user
 
     def __str__(self):
