0f797de8c5fa2d082e7b053270e09d308c6fb9e2
ShaniStrassProg <37214725715@mby.co.il>
2024-09-02 19:07:55 +0300

    add is_exit_permission

--- DB_UserAdministration/DataAccess/permissionManager.py
+++ DB_UserAdministration/DataAccess/permissionManager.py
@@ -1,9 +1,9 @@
 from permissionModel import Action
+import sqlite3
 # from DataAccess impotr DBManager
 class permissionManager:
 
     def __init__(self, db_file: str):
-
         '''Initialize permissionManager with the database connection.'''
         self.db_manager = DBManager(db_file)
         self.table_name ='permission_manager'
@@ -11,6 +11,19 @@ class permissionManager:
 
     def create_permission(self,permission):
         self.db.insert(self.table_name, permission)
+
+    def is_exit_permission(self, action, resource, effect):
+        '''Check if an permission with these given values exists in the database.'''
+        query = f'SELECT 1 FROM {self.table_name} WHERE metadata LIKE ? LIMIT 1'
+        try:
+            c = self.db_manager.connection.cursor()
+            c.execute(query, (f'%\'action\': \'{action}\'%
+                             %\'resource\': \'{resource}\'%
+                            %\'effect\': \'{effect}\'%',))
+            result = c.fetchone()
+            return result is not None
+        except sqlite3.OperationalError as e:
+            raise Exception(f'Error checking for option group existence: {e}')
 
 
     def create_table(self):
--- DB_UserAdministration/Services/permissionService.py
+++ DB_UserAdministration/Services/permissionService.py
@@ -7,20 +7,22 @@ class permissionService:
         self.dal = dal
 
     def create_permission(self, action:Action, resource: Resource, effect:Effect):
+        if self.is_exit_permission(action, resource, effect):
+             raise ValueError(f'permission \'{action}, {resource}, {effect}\' already exists.')
         new_permission = Permission(action, resource, effect)
         return self.dal.create_permission(new_permission.to_dict())
+    
+    def is_exit_permission(self, action, resource, effect):
+        return self.dal.is_exit_permission(action, resource, effect)
     
     def delete_permission(self, permission_id: int):
         return self.dal.delete_permission(permission_id)
 
-    def list_permissions(self):
-        return self.list_permissions()
+    # def list_permissions(self):
+    #     return self.list_permissions()
 
     def update_permission(self, permission_id: int, action:Action, resource: Resource ):
         return self.dal.update_permission(permission_id, action, resource)
 
-    def get_permission(self, permission_id: int):
-        return self.dal.get_permission(permission_id)
-
-    def has_permission(self, action:Action, resource: Resource):
-        return self.dal.has_permission(action, resource)
+    # def get_permission(self, permission_id: int):
+    #     return self.dal.get_permission(permission_id)
