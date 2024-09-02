2e5ce59e41ca638d563bafa3518d09147b4db301
Tem M <60137811+Tem-M@users.noreply.github.com>
2024-09-02 16:26:20 +0300

    Revert "Iam policies"

--- DB/Validation/PolicyValidation.py
+++
@@ -1,7 +0,0 @@
-from DB.DataAccess.PolicyManager import PolicyManager
-
-
-def validate_policy_name(dal: PolicyManager, policy_name: str):
-    if not dal.is_identifier_exist(policy_name):
-        raise ValueError(f'Policy {policy_name} does not exist')
-
