b8ff112e4cb029c36accaae9b4419c3923b50366
Tem-M <temima628@gmail.com>
2024-09-02 13:25:09 +0300

    policy evaluate

--- DB_UserAdministration/Models/PolicyModel.py
+++ DB_UserAdministration/Models/PolicyModel.py
@@ -2,16 +2,16 @@ from typing import List
 
 
 class Policy:
-    def __init__(self, name: str, permissions: List[Permission] = None): 
+    def __init__(self, name: str, permissions: List[Permission] = None):
         self.name = name
         self.permissions = permissions if permissions != None else []
-        
+
     def to_dict(self):
         return {
             "policy_id": self.name,
-            "permissions": [perm.to_dict() for perm in self.permissions]
+            "permissions": [perm.to_dict() for perm in self.permissions],
         }
-        
+
     def evaluate(self, action, resource):
         allowed = False
         denied = False
@@ -19,13 +19,10 @@ class Policy:
         # the evaluate function returns wether non of the policy permissions deny access
         # and at least one permits access
         for permission in self.permissions:
-            if permission.evaluate(action, resource) == 'Allow':
-                allowed = True
-            elif permission.evaluate(action, resource) == 'Deny':
-                denied = True
-            else:
-                pass
-            
-        return allowed and not denied
-        
-        
+            if permission.resource == resource and permission.action == action:
+                if permission.effect == "Deny":
+                    return False
+                elif permission.effect == "Allow":
+                    allowed = True
+
+        return allowed
