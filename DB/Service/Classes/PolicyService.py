10f9d3a382cec77af302f80446bbb797081c0cff
Tem-M <temima628@gmail.com>
2024-09-02 11:22:58 +0300

    policy evaluate basic implementation

--- DB/Service/Classes/PolicyService.py
+++ DB/Service/Classes/PolicyService.py
@@ -1,4 +1,5 @@
 from sqlite3 import OperationalError
+from typing import Dict, List
 from DB.DataAccess import PolicyManager
 from DB.Models.PolicyModel import Policy
 
@@ -6,6 +7,7 @@ from DB.Models.PolicyModel import Policy
 class PolicyService:
     def __init__(self, policy_manager: PolicyManager):
         self.policy_manager = policy_manager
+        self.policies: Dict[str, Policy] = {}
         
     def create(self, name):
         new_policy = Policy(name)
@@ -13,6 +15,7 @@ class PolicyService:
             self.policy_manager.create(new_policy.to_dict())
         except OperationalError as e:
             raise ValueError(f'An internal error occurred: {str(e)}') 
+        self.policies[name] = new_policy
         
     def delete(self, name):
         try:
@@ -20,12 +23,16 @@ class PolicyService:
         except OperationalError as ex:
             return 'policy couldn\'t be deleted'
         
+        del self.policies[name]
+        
     
-    def update(self, name, permissions):
+    def update(self, name, permissions): 
+        updated_policy = Policy(name, permissions=permissions)
         try:
-            self.policy_manager.update(name, Policy(name, permissions=permissions))
+            self.policy_manager.update(name, updated_policy)
         except OperationalError as ex:
             return 'policy couldn\'t be updated'
+        self.policies[name] = updated_policy
         
     def get(self, name):
         try:
@@ -35,6 +42,17 @@ class PolicyService:
         
     def list_policies(self):
         try:
-            return self.policy_manager.get_all_policies()
+            return self.policies
         except OperationalError as ex:
             return 'policy list couldn\'t be retrieved'
+        
+    def evaluate(self, action, resource, conditions):
+        allowed = False
+        denied = False
+        for policy in self.policies:
+            for permission in policy.permissions:
+                if permission.evaluate(action, resource, conditions):
+                    allowed = True
+                else:
+                    denied = True
+        return allowed or not denied
