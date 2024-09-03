98eef4a22dc981b3b87989cf0d7725a7e8673b30
shana-git <shana3229317@gmail.com>
2024-09-03 17:02:47 +0300

    remove json

--- Storage_UserAdministration/Controllers/AuthenticationController.py
+++ Storage_UserAdministration/Controllers/AuthenticationController.py
@@ -43,10 +43,10 @@ class AuthenticationController:
 if __name__ == "__main__":
     controller = AuthenticationController()
     # # Register a user
-    controller.register('user', 'password123')
+    controller.register('user1', 'password123')
     # # Login a user
-    session_id = controller.login('user', 'password123')
+    session_id = controller.login('user1', 'password123')
     # Check if the user is authenticated
-    controller.check_authentication('user')
+    controller.check_authentication('user1')
     # Logout the user
-    controller.logout('user')
+    controller.logout('user1')
