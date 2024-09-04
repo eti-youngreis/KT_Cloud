b84bc7dac54dc246e651156633cca316a1327b9d
efrat <efrat7248.com>
2024-09-04 15:04:06 +0300

    save user

--- Storage_UserAdministration/Models/userModel.py
+++ Storage_UserAdministration/Models/userModel.py
@@ -1,8 +1,9 @@
-# from Models.PermissionModel import Permission
-# from Models.PolicyModel import Policy
-# from Models.GroupModel import Group
-# from Models.QuotaModel import Quota
-# from Models.RoleModel import Role
+
+from Models.PermissionModel import Permission
+from Models.PolicyModel import Policy
+from Models.GroupModel import Group
+from Models.QuotaModel import Quota
+from Models.RoleModel import Role
 import uuid
 
 class User:
@@ -10,42 +11,39 @@ class User:
         self,
         username: str,
         password: str,
+        user_id = None,
         email=None,
-        
-        # role: Optional[Role] = None,
-        # policies: Optional[List[Policy]] = None,
-        # quota: Optional[Quota] = None,
-        # groups: Optional[List[Group]] = None
+        logged_in = False,
+        token = None,
+        role: Optional[Role] = None,
+        policies: Optional[List[Policy]] = None,
+        quota: Optional[Quota] = None,
+        groups: Optional[List[Group]] = None
     ):
         self.user_id = str(uuid.uuid4())  # Unique identifier
         self.username = username
         self.password=password
         self.email = email
-        self.logged_in=False
-        self.token=None
-        # self.role = role
-        # self.policies = policies
-        # self.quota =quota
-        # self.groups = groups
-
-
-    # def hash_password(self, password):
-    #     # Implement password hashing
-    #     return hashlib.sha256(password.encode()).hexdigest()
+        self.logged_in=logged_in
+        self.token=token
+        self.role = role
+        self.policies = policies
+        self.quota =quota
+        self.groups = groups
 
     def verify_password(self, password:str):
         # Verify password against the hashed password
         return self.password_hash == self.hash_password(password)
 
-    # def can(self, action, resource):
-    #     return any(
-    #         policy.evaluate(policy_name, permissions) for policy in self.policies
-    #     ) or self.role.has_permission(permissions)
+    def can(self, action, resource):
+        return any(
+            policy.evaluate(policy_name, permissions) for policy in self.policies
+        ) or self.role.has_permission(permissions)
 
-    # def update_quota(self, quota: Quota):
-    #     self.quota = quota
+    def update_quota(self, quota: Quota):
+        self.quota = quota
 
-    # def check_quota(self):
-    #     return self.quota.check_exceeded()
+    def check_quota(self):
+        return self.quota.check_exceeded()
 
 
