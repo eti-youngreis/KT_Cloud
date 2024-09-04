ca34cd5c0a7f8db8d8c019821ab5e6d0fa77806f
Tamar-Gav <37325928810@mby.co.il>
2024-09-04 13:33:08 +0300

    Add the to_dict function in the models: Acl, Encryption, object, Tag and version

--- Storage/Models/ObjectModel.py
+++ Storage/Models/ObjectModel.py
@@ -1,15 +1,23 @@
-from .VesionModel import Version
-from .EncryptionModel import EncryptionModel
+from VesionModel import Version
+from EncryptionModel import EncryptionModel
 from Tag import Tag
+from AclModel import Acl
 
 class ObjectModel:
     def __init__(self, bucket_name: str, object_key: str):
         self.bucket = bucket_name
         self.key = object_key
-        # self.tagging = Tagging()
-        self.versioning:list[Version] = []
-        # self.acl = ACL()
-    
+        self.tagging:list[Tag] = []
+        self.versioning: list[Version] = []
+        self.acl = Acl()
         self.encryption = EncryptionModel()
-        # self.acl = ACL()
 
+    def to_dict(self):
+        return {
+            "Bucket": self.bucket,
+            "Key": self.key,
+            "TagSet": [tag.to_dict() for tag in self.tagging],
+            "Versions": [version.to_dict() for version in self.versioning],
+            "Acl": self.acl.to_dict(),
+            "Encryption": self.encryption.to_dict(),
+        }
