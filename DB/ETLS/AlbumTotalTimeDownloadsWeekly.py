3d80c929d5c621b1b6ad5d199a77d39ee17fa70d
Tem-M <temima628@gmail.com>
2024-09-08 17:49:07 +0300

    fix according to comment

--- DB/ETLS/AlbumTotalTimeDownloadsWeekly.py
+++ DB/ETLS/AlbumTotalTimeDownloadsWeekly.py
@@ -9,16 +9,23 @@ import sqlite3
 
 def load():
 
+    # configure basic variables
     current_user = "User" # when IAM is implemented, get current user for session details
     
     etl_table_name = 'album_total_time_downloads'
     
     base_path = "../etl_files/"
+    
+    # establish sparkSession
     spark = SparkSession.builder.appName("ETL Template with SQLite").getOrCreate()
 
     try:
-    
+        
+        # connect to database
+        
         conn = sqlite3.connect(base_path + "database.db")
+        
+        # extract data from csv files
         album_table = spark.read.option("header", "true").csv(base_path + "Album.csv")
 
         track_table = spark.read.option("header", "true").csv(base_path + "Track.csv")
@@ -27,6 +34,7 @@ def load():
             base_path + "InvoiceLine.csv"
         )
 
+        # join tables in order to transform data
         joined_album_track_table = album_table.join(
             track_table, album_table["AlbumId"] == track_table["AlbumId"], "inner"
         ).drop(track_table["AlbumId"])
@@ -36,15 +44,18 @@ def load():
             "inner",
         ).drop(invoice_line_table["TrackId"])
 
+        # aggregate data to derive conclusions
         aggregated_data = joined_album_track_invoice_line_table.groupBy("AlbumId").agg(
             F.sum("Milliseconds").alias("total_album_length"),
             F.sum("Quantity").alias("total_album_downloads"),
         )
-                
+        
+        # transformed data including metadata 
         final_data = aggregated_data.withColumn("created_at", F.current_date()) \
             .withColumn("updated_at", F.current_date()) \
             .withColumn("updated_by", F.lit(f"DailyAlbumETL:{current_user}"))
     
+        # load data to database
         final_data = final_data.toPandas()
             
         final_data.to_sql(name=etl_table_name, con = conn, if_exists='replace', index=False)   
--- DB/ETLS/RevenuePerCustomerGenreWeekly.py
+++ DB/ETLS/RevenuePerCustomerGenreWeekly.py
@@ -6,22 +6,30 @@ import pandas as pd
 import sqlite3
 
 def load():
+    
+    # configure basic variables
     current_user = "User" # when IAM is implemented, get current user for session details
     
     etl_table_name = 'revenue_per_customer_genre'
     
     base_path = "../etl_files/"
+    
+    # establish sparkSession
     spark = SparkSession.builder.appName("ETL Template with SQLite").getOrCreate()
-
+    
     
     try:
+        # connect to sqlite database
         conn = sqlite3.connect(base_path + "database.db")
+        
+        # extract data from csv files
         customer_table = spark.read.option("header", "true").csv(base_path + "Customer.csv")
         invoice_table = spark.read.option("header", "true").csv(base_path + "Invoice.csv")
         invoice_line_table = spark.read.option("header", "true").csv(base_path + "InvoiceLine.csv")
         track_table = spark.read.option("header", "true").csv(base_path + "Track.csv")
         genre_table = spark.read.option("header", "true").csv(base_path + "Genre.csv")
         
+        # join tables in order to transform data
         customer_invoices_table = customer_table.join(invoice_table, customer_table['CustomerId'] == \
             invoice_table['CustomerId']).drop(invoice_table['CustomerId'])
             
@@ -32,7 +40,7 @@ def load():
         
         customer_genre_table = customer_track_table.join(genre_table, customer_track_table['GenreId'] == genre_table['GenreId']).drop(customer_track_table['GenreId'])
 
-        
+        # aggregate data
         customer_genre_table = customer_genre_table.withColumn(
             'invoice_line_total', 
             F.col('UnitPrice') * F.col('Quantity')
@@ -42,10 +50,12 @@ def load():
             F.sum("invoice_line_total").alias("revenue_overall")
         )
         
+        # transformed data including metadata 
         final_data = aggregated_data.withColumn("created_at", F.current_date()) \
             .withColumn("updated_at", F.current_date()) \
             .withColumn("updated_by", F.lit(f"WeeklyCustomerRevenuePerGenre:{current_user}"))
-    
+
+        # load data to database
         final_data = final_data.toPandas()
         
         final_data.to_sql(name=etl_table_name, con = conn, if_exists='replace', index=False)   
