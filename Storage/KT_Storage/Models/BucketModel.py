80bb903b23f257493c8ee2ea670db8f02d0d640e
תמר מליק <tamar.m1225@gmail.com>
2024-09-09 00:29:20 +0300

    rename ETLs Files

--- Storage/KT_Storage/Models/BucketModel.py
+++ Storage/KT_Storage/Models/BucketModel.py
@@ -14,11 +14,11 @@ class Bucket:
         self.cors_configuration = cors_configuration
         self.encrypt_mode = False
         
-    # def is_encrypted(self):
-    #     return self.encrypt_mode
+    def is_encrypted(self):
+        return self.encrypt_mode
     
-    # def update_encryption(self, is_encrypted):
-    #     self.encrypt_mode = is_encrypted
+    def update_encryption(self, is_encrypted):
+        self.encrypt_mode = is_encrypted
 
     # def to_dict(self):
     #     return{
