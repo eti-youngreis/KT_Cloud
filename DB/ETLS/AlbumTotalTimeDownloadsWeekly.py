b09a1e9fe8462c5a03ee34935a8c968261d0a15c
Tem-M <temima628@gmail.com>
2024-09-08 20:01:21 +0300

    changes after validation

--- DB/ETLS/AlbumTotalTimeDownloadsWeekly.py
+++ DB/ETLS/AlbumTotalTimeDownloadsWeekly.py
@@ -26,38 +26,45 @@ def load():
         conn = sqlite3.connect(base_path + "database.db")
         
         # extract data from csv files
-        album_table = spark.read.option("header", "true").csv(base_path + "Album.csv")
-
         track_table = spark.read.option("header", "true").csv(base_path + "Track.csv")
 
         invoice_line_table = spark.read.option("header", "true").csv(
             base_path + "InvoiceLine.csv"
         )
+        
+        # total time per album
+        total_time_per_album = track_table.groupBy("AlbumId").agg(
+            F.sum("Milliseconds").alias("total_album_length")
+        )
 
-        # join tables in order to transform data
-        joined_album_track_table = album_table.join(
-            track_table, album_table["AlbumId"] == track_table["AlbumId"], "inner"
-        ).drop(track_table["AlbumId"])
-        joined_album_track_invoice_line_table = joined_album_track_table.join(
+        # total downloads per album
+        joined_track_invoice_line_table = track_table.join(
             invoice_line_table,
-            joined_album_track_table["TrackId"] == invoice_line_table["TrackId"],
-            "inner",
+            track_table["TrackId"] == invoice_line_table["TrackId"],
+            "left",
         ).drop(invoice_line_table["TrackId"])
-
-        # aggregate data to derive conclusions
-        aggregated_data = joined_album_track_invoice_line_table.groupBy("AlbumId").agg(
-            F.sum("Milliseconds").alias("total_album_length"),
+        
+        total_downloads_per_album = joined_track_invoice_line_table.groupBy("AlbumId").agg(
             F.sum("Quantity").alias("total_album_downloads"),
         )
+
+    
+        # join derived conclusions to final data
+        final_data = total_time_per_album.join(total_downloads_per_album, total_time_per_album["AlbumId"] ==\
+            total_downloads_per_album["AlbumId"]).drop(total_downloads_per_album["AlbumId"])
+        
         
         # transformed data including metadata 
-        final_data = aggregated_data.withColumn("created_at", F.current_date()) \
+        final_data = final_data.withColumn("created_at", F.current_date()) \
             .withColumn("updated_at", F.current_date()) \
             .withColumn("updated_by", F.lit(f"DailyAlbumETL:{current_user}"))
     
         # load data to database
         final_data = final_data.toPandas()
-            
+        
+        
+        final_data['AlbumId'] = final_data['AlbumId'].astype(int)
+    
         final_data.to_sql(name=etl_table_name, con = conn, if_exists='replace', index=False)   
         
     finally:
