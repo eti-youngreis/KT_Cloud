a51a498d9a9bb072d01a1266cee05f21eb78c832
Mychaly <mi3238086@gmail.com>
2024-09-11 13:46:08 +0300

    improve the incremental implementation

--- Storage/ETLS/AlbumPopularityAndRevenueDailyETL.py
+++ Storage/ETLS/AlbumPopularityAndRevenueDailyETL.py
@@ -17,12 +17,12 @@ def add_created_at_column(file_path):
         df['updated_at'] = current_time
 
 
-        new_file_path = file_path  
+        new_file_path = file_path
         df.to_csv(new_file_path, index=False)
 
         print(f"File saved with 'created_at' column: {new_file_path}")
 
-def incremental_load():
+def load_album_popularity_and_revenue_incremental_ETL():
     # Step 1: Initialize Spark session
     spark = SparkSession.builder \
         .appName("Album Popularity and Revenue ETL") \
@@ -59,26 +59,38 @@ def incremental_load():
 
         # filter data by updated_at
         albums = albums.withColumn("updated_at", to_timestamp(col("updated_at"), "dd/MM/yyyy HH:mm"))
-        albums = albums.where(albums["updated_at"] > latest_timestamp)
-
-
+        # albums = albums.where(albums["updated_at"] > latest_timestamp)
+        invoice_lines = invoice_lines.withColumn("updated_at", to_timestamp(col("updated_at"), "dd/MM/yyyy HH:mm"))
+        # invoice_lines = invoice_lines.where(invoice_lines["updated_at"] > latest_timestamp)
+        tracks = tracks.withColumn("updated_at", to_timestamp(col("updated_at"), "dd/MM/yyyy HH:mm"))
+        # tracks = tracks.where(tracks["updated_at"] > latest_timestamp)
 
 
         # Rename columns to avoid ambiguity
-        albums = albums.withColumnRenamed("AlbumId", "AlbumId_Albums")
-        tracks = tracks.withColumnRenamed("AlbumId", "AlbumId_Tracks").withColumnRenamed("UnitPrice", "Track_UnitPrice")
+        albums = albums.withColumnRenamed("AlbumId", "AlbumId_Albums").withColumnRenamed("updated_at", "updated_at_albums")
+        tracks = tracks.withColumnRenamed("AlbumId", "AlbumId_Tracks").withColumnRenamed("UnitPrice", "Track_UnitPrice").withColumnRenamed("updated_at", "updated_at_tracks")
         invoice_lines = invoice_lines.withColumnRenamed("TrackId", "TrackId_InvoiceLines").withColumnRenamed(
-            "UnitPrice", "Invoice_UnitPrice")
+            "UnitPrice", "Invoice_UnitPrice").withColumnRenamed("updated_at", "updated_at_invoice_lines")
 
-        # TRANSFORM
+
+
         # Join albums with tracks to get album-related information
-        album_tracks = albums.join(tracks, albums["AlbumId_Albums"] == tracks["AlbumId_Tracks"], "inner")
+        album_tracks = albums.join(
+            tracks,
+            (albums["AlbumId_Albums"] == tracks["AlbumId_Tracks"]) &
+            ((albums["updated_at_albums"] > latest_timestamp) |
+             (tracks["updated_at_tracks"] > latest_timestamp)),
+            "inner"
+        )
 
         # Join the result with invoice_lines to get revenue details
-        album_invoice_lines = album_tracks.join(invoice_lines,
-                                                album_tracks["TrackId"] == invoice_lines["TrackId_InvoiceLines"],
-                                                "inner")
-
+        album_invoice_lines = album_tracks.join(
+            invoice_lines,
+            (album_tracks["TrackId"] == invoice_lines["TrackId_InvoiceLines"]) &
+            ((album_tracks["updated_at_albums"] > latest_timestamp) |
+             (invoice_lines["updated_at_invoice_lines"] > latest_timestamp)),
+            "inner"
+        )
 
         # Calculate total revenue and track count per album
         album_revenue = album_invoice_lines.groupBy("AlbumId_Albums", "Title", "ArtistId") \
@@ -86,6 +98,11 @@ def incremental_load():
             F.sum("Invoice_UnitPrice").alias("TotalRevenue"),
             F.count("TrackId").alias("TrackCount")
         )
+
+
+
+        # הדפסת מספר השורות
+        print(f"Number of rows : {album_revenue.count()}")
 
 
         # Rename columns back to original names if needed
@@ -96,30 +113,28 @@ def incremental_load():
             .withColumn("updated_at", F.lit(now)) \
             .withColumn("updated_by", F.lit("process:user_name"))
 
-
         rows = album_revenue.collect()
 
         for row in rows:
             cursor.execute('''
-                UPDATE AlbumPopularityAndRevenue
-                SET Title = ?,
-                    ArtistId = ?,
-                    TotalRevenue = ?,
-                    TrackCount = ?,
-                    updated_at = ?
-                WHERE AlbumId = ?
-            ''', (row.Title, row.ArtistId, row.TotalRevenue, row.TrackCount, row.updated_at, row.AlbumId))
-
+                        UPDATE AlbumPopularityAndRevenue
+                        SET Title = ?,
+                            ArtistId = ?,
+                            TotalRevenue = ?,
+                            TrackCount = ?,
+                            updated_at = ?
+                        WHERE AlbumId = ?
+                    ''', (row.Title, row.ArtistId, row.TotalRevenue, row.TrackCount, row.updated_at, row.AlbumId))
 
         cursor.executemany('''
-            INSERT INTO AlbumPopularityAndRevenue (AlbumId, Title, ArtistId, TotalRevenue, TrackCount, created_at, updated_at)
-            SELECT ?, ?, ?, ?, ?, ?, ?
-            WHERE NOT EXISTS (
-                SELECT 1 FROM AlbumPopularityAndRevenue WHERE AlbumId = ?
-            )
-        ''', [(row.AlbumId, row.Title, row.ArtistId, row.TotalRevenue, row.TrackCount, row.created_at, row.updated_at,
-               row.AlbumId) for row in rows])
-
+                    INSERT INTO AlbumPopularityAndRevenue (AlbumId, Title, ArtistId, TotalRevenue, TrackCount, created_at, updated_at)
+                    SELECT ?, ?, ?, ?, ?, ?, ?
+                    WHERE NOT EXISTS (
+                        SELECT 1 FROM AlbumPopularityAndRevenue WHERE AlbumId = ?
+                    )
+                ''', [
+            (row.AlbumId, row.Title, row.ArtistId, row.TotalRevenue, row.TrackCount, row.created_at, row.updated_at,
+             row.AlbumId) for row in rows])
 
         conn.commit()
 
@@ -127,5 +142,6 @@ def incremental_load():
 
         conn.close()
 
+
 if __name__ == "__main__":
-    incremental_load()
+    load_album_popularity_and_revenue_incremental_ETL()
