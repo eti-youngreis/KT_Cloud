1645f32d51999d41d62166e296a89aeeccac0d29
shana-git <shana3229317@gmail.com>
2024-09-11 21:38:09 +0300

    add airflow

--- Storage/Orchestration/ELTWeeklyProcess.py
+++ Storage/Orchestration/ELTWeeklyProcess.py
@@ -3,6 +3,8 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 from ..ELTS.CustomerPurchaseFrequencyTotalSpendWeeklyELT import load_elt
 from ..ELTS.topSellingArtistsWeeklyELT import load_and_transform_data
+from ..ELTS.employeeSalePerformanceCustomerInteractionsWeeklyELT import load_employees_sales_customer_interactions_elt
+from ..ELTS.CustomerAverageSpendWeeklyELT import load_average_purchase_value_elt
 # from ELTS import X
 
 # Define your Python functions here
@@ -23,6 +25,12 @@ def run_customer_purchase_frequency_total_spend():
     load_elt()
 def run_top_sell_artists():
     load_and_transform_data()
+
+def run_employees_sales_customer_interactions():
+    load_employees_sales_customer_interactions_elt()
+
+def run_customer_invoices_count():
+    load_average_purchase_value_elt()
 
 # Define default arguments for the DAG
 default_args = {
@@ -74,6 +82,14 @@ with DAG(
     task_run_top_sell_artists(
         task_id='run_top_sell_artists',
         python_callable=run_top_sell_artists,
+    )
+    task_employees_sales_customer_interactions(
+        task_id='run_employees_sales_customer_interactions',
+        python_callable=run_employees_sales_customer_interactions,
+    )
+    task_average_purchase_value_elt(
+        task_id='run_customer_invoices_count_etl',
+        python_callable=run_customer_invoices_count,
     )
     # Define dependencies
     task_1 >> task_2
