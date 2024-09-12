439817d5057d60b31c425a0e8eac48c42bac1e52
Tamar-Gav <37325928810@mby.co.il>
2024-09-12 11:31:12 +0300

    a few changes in KT_STORAGE

--- DB/Orchestration/ETLWeeklyProcess.py
+++ DB/Orchestration/ETLWeeklyProcess.py
@@ -1,62 +1,74 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from Storage.ETLS import GenreSalesWeeklyETL
+
 # from ETLS import X
+
+
 
 # Define your Python functions here
 def run_table_1():
-    # Code to generate Table 1
-    pass
+    GenreSalesWeeklyETL.load
+
 
 def run_table_2():
     # Code to generate Table 2
     pass
 
+
 def run_table_3():
     # Code to generate Table 3
     pass
+
 
 # More functions for other tasks as necessary
 
 # Define default arguments for the DAG
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 9, 1),
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
+    "owner": "airflow",
+    "start_date": datetime(2024, 9, 1),
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
 }
 
 # Initialize DAG
 with DAG(
-    'etl_orchestration',
+    "etl_orchestration",
     default_args=default_args,
-    description='ETL Process Orchestration DAG',
+    description="ETL Process Orchestration DAG",
     schedule_interval=None,  # Set to None for manual runs
     catchup=False,
 ) as dag:
 
     # Task 1 (Independent tasks that run first)
     task_1 = PythonOperator(
-        task_id='run_table_1',
+        task_id="run_table_1",
         python_callable=run_table_1,
     )
-    
+
     task_3 = PythonOperator(
-        task_id='run_table_3',
+        task_id="run_table_3",
         python_callable=run_table_3,
     )
-    
+
     # Add more independent tasks here
     task_5 = PythonOperator(
-        task_id='run_table_5',
+        task_id="run_table_5",
         python_callable=run_table_5,
     )
 
     # Dependent tasks that run after Table 1, 3, 5
     task_2 = PythonOperator(
-        task_id='run_table_2',
+        task_id="run_table_2",
         python_callable=run_table_2,
     )
 
