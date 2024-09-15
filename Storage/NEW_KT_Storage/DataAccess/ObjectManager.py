6899fcdd682d4c49b7c9db1579384ea906e8a0e9
malkimarxon <m0548501844@gmail.com>
2024-09-15 22:31:11 +0300

    changes in the init

--- Storage/NEW_KT_Storage/DataAccess/ObjectManager.py
+++ Storage/NEW_KT_Storage/DataAccess/ObjectManager.py
@@ -5,18 +5,18 @@ from DB.NEW_KT_DB.DataAccess.ObjectManager import ObjectManager
 from StorageManager import StorageManager
 
 class ObjectManager:
-    def __init__(self, db_file: str, storage_path=None):
+    def __init__(self, db_file: str,type, storage_path=None):
         """Initialize ObjectManager with the database connection."""
         self.object_manager = ObjectManager(db_file)
         self.storage_manager = StorageManager(storage_path)
-
+        self.object_name = type
 
     # for outer use:
-    def save_in_memory(self, object, name):
+    def save_in_memory(self, object):
        
         # insert object info into management table mng_{object_name}s
         # for exmple: object db_instance will be saved in table mng_db_instances
-        table_name = self.object_manager.convert_object_name_to_management_table_name(name)
+        table_name = self.object_manager.convert_object_name_to_management_table_name(self.object_name)
 
         if not self.object_manager.is_management_table_exist(table_name):
             self.object_manager.create_management_table(table_name)
@@ -24,24 +24,24 @@ class ObjectManager:
         self.object_manager.insert_object_to_management_table(table_name, object)
 
     
-    def delete_from_memory(self,object, name, criteria='default'):
+    def delete_from_memory(self,object, criteria='default'):
         
         # if criteria not sent- use PK for deletion
         if criteria == 'default':
             criteria = f'{object.pk_column} = {object.pk_value}'
         
-        table_name = self.object_manager.convert_object_name_to_management_table_name(name)
+        table_name = self.object_manager.convert_object_name_to_management_table_name(self.object_name)
         
         self.object_manager.delete_data_from_table(table_name, criteria)
 
 
-    def update_in_memory(self,object,name, updates, criteria='default'):
+    def update_in_memory(self,object, updates, criteria='default'):
         
         # if criteria not sent- use PK for deletion
         if criteria == 'default':
             criteria = f'{object.pk_column} = {object.pk_value}'
 
-        table_name = self.object_manager.convert_object_name_to_management_table_name(name)
+        table_name = self.object_manager.convert_object_name_to_management_table_name(self.object_name)
 
         self.object_manager.update_object_in_management_table_by_criteria(table_name, updates, criteria)
 
@@ -59,5 +59,3 @@ class ObjectManager:
             dict[key] = value
     
         return dict
-
-
