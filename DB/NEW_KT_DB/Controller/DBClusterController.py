aa3d696e8d01ecb15467edea3d154a5a26833ef2
sara-lea <m3197522@gmail.com>
2024-09-16 16:15:44 +0300

    try to run the file

--- DB/NEW_KT_DB/Controller/DBClusterController.py
+++ DB/NEW_KT_DB/Controller/DBClusterController.py
@@ -1,5 +1,5 @@
-from Service import DBClusterService
-
+# from Service.Classes import DBClusterService
+from Service.Classes.DBClusterService import DBClusterService
 class DBClusterController:
     def __init__(self, service: DBClusterService):
         self.service = service
@@ -15,4 +15,15 @@ class DBClusterController:
 
     def modify_db_cluster(self, updates):
         self.service.modify(updates)
-    
+
+
+if __name__=='__main__':
+    clusterController = DBClusterController()
+    cluster_data = {
+    'db_cluster_identifier': 'my-cluster-1',
+    'engine': 'aurora-mysql',
+    'db_subnet_group_name': 'my-subnet-group'
+    }
+
+    aaa = clusterController.create_db_cluster(**cluster_data)
+    print(aaa)
