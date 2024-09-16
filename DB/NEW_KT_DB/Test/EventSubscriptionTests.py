359fd6c45e4812d6c36c7383bb879e4120c6b9d7
etiyoungreis <etiyoungreis@gmail.com>
2024-09-16 16:25:20 +0300

    Use mocking in EventSubscriptionTest

--- DB/NEW_KT_DB/Test/EventSubscriptionTests.py
+++ DB/NEW_KT_DB/Test/EventSubscriptionTests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from DB.NEW_KT_DB.DataAccess.EventSubscriptionManager import EventSubscriptionManager
 from DB.NEW_KT_DB.Controller.EventSubscriptionController import EventSubscriptionController
 from DB.NEW_KT_DB.Models.EventSubscriptionModel import SourceType, EventCategory
@@ -5,12 +7,28 @@ from DB.NEW_KT_DB.Service.Classes.EventSubscriptionService import EventSubscript
 from Storage.NEW_KT_Storage.DataAccess.StorageManager import StorageManager
 
 
-def test_create_with_valid_input():
+@pytest.fixture
+def mock_event_subscription_manager(mocker):
+    return mocker.Mock()
+
+
+@pytest.fixture
+def event_subscription_service(mocker_event_subscription_manager):
+    return EventSubscriptionService(
+        dal=mock_event_subscription_manager,
+        storage_manager=StorageManager('test_storage'),
+        directory='test_directory'
+    )
+
+
+def test_create_with_valid_input(event_subscription_service, mock_event_subscription_manager):
     '''Test the create method.'''
-    # create a new EventSubscription
+
+    # Set up the mock to return a predefined response for the create method
+
     event_subscription_controller = EventSubscriptionController(
-        service=EventSubscriptionService(
-            dal=EventSubscriptionManager(db_file='test_db.db'), storage_manager=StorageManager(base_directory='test_storage'), directory='test_directory'))
+        event_subscription_service
+    )
 
     event_subscription_controller.create_event_subscription(
         subscription_name='test_subscription',
@@ -22,6 +40,7 @@ def test_create_with_valid_input():
         sns_topic_arn='test_sns_topic_arn',
         source_type=SourceType.DB_INSTANCE
     )
+    
     # check that the EventSubscription was created
     event_subscription = event_subscription_controller.get(
         subscription_name='test_subscription')
