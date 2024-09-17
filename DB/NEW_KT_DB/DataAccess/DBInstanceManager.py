391c50e0206ed7ffa5aa50669eb34d9f51b56644
sarit <sarit5rot@gmail.com>
2024-09-17 13:10:02 +0300

    change  DBInstanceManager

--- DB/NEW_KT_DB/DataAccess/DBInstanceManager.py
+++ DB/NEW_KT_DB/DataAccess/DBInstanceManager.py
@@ -8,7 +8,6 @@ class DBInstanceManager:
         self.object_manager = ObjectManager(db_file)
         self.table_name = 'db_instance'
       
-
     def createInMemoryDBInstance(self, db_instance):
         metadata = json.dumps(db_instance.to_dict())
         data = (db_instance.db_instance_identifier, metadata)
@@ -16,31 +15,25 @@ class DBInstanceManager:
 
     def modifyDBInstance(self, db_instance):
         metadata = json.dumps(db_instance.to_dict())
-        updates = {'metadata': metadata}
-        criteria = f"db_instance_identifier = '{
-            db_instance.db_instance_identifier}'"
-        self.object_manager.update_in_memory(
-            type_object='db_instance', updates=updates, criteria=criteria)
+        updates = f"metadata = '{metadata}'"
+        criteria = f"db_instance_identifier = '{db_instance.db_instance_identifier}'"
+        self.object_manager.update_in_memory(self.table_name, updates, criteria)
 
     def describeDBInstance(self, db_instance_identifier) -> Dict[str, Any]:
         criteria = f"db_instance_identifier = '{db_instance_identifier}'"
-        result = self.object_manager.get_from_memory(
-            type_object='db_instance', columns=["*"], criteria=criteria)
+        result = self.object_manager.get_from_memory(self.table_name, ["*"], criteria)
 
         if result:
-            metadata = json.loads(result['metadata'])
+            metadata = json.loads(result[0][1])  
             return metadata
         else:
             raise ValueError(f"DB Instance with identifier { db_instance_identifier} not found.")
 
     def deleteInMemoryDBInstance(self, db_instance_identifier):
         criteria = f"db_instance_identifier = '{db_instance_identifier}'"
-        self.object_manager.delete_from_memory(
-            self.table_name, criteria)
+        self.object_manager.delete_from_memory_by_criteria(self.table_name, criteria)
 
     def getDBInstance(self, db_instance_identifier):
         criteria = f"db_instance_identifier = '{db_instance_identifier}'"
-        result = self.object_manager.get_from_memory(
-            type_object=self.table_name, columns=["*"], criteria=criteria)
+        result = self.object_manager.get_from_memory(self.table_name, ["*"], criteria)
 
-        # להחיות את האובייקט    
