8e6777e0a3b5a48ad1620fbfdf846c3d664b8411
etiyoungreis <etiyoungreis@gmail.com>
2024-09-17 16:20:49 +0300

    Updated EventSubscriptionManager to use functions from ObjectManager correctly

--- DB/NEW_KT_DB/DataAccess/EventSubscriptionManager.py
+++ DB/NEW_KT_DB/DataAccess/EventSubscriptionManager.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
 from DB.NEW_KT_DB.Models.EventSubscriptionModel import EventSubscription
 from DB.NEW_KT_DB.DataAccess.ObjectManager import ObjectManager
 
@@ -11,22 +11,21 @@ class EventSubscriptionManager:
         self.object_manager.create_management_table(
             EventSubscription.get_object_name(), EventSubscription.table_schema)
 
-    def createInMemoryEventSubscription(self, event_subscription: EventSubscription):
+    def createInMemoryEventSubscription(self, event_subscription: EventSubscription) -> None:
         self.object_manager.save_in_memory(event_subscription.get_object_name(), event_subscription.to_sql(
         ))
 
-    def deleteInMemoryEventSubscription(self, subscription_name: str):
+    def deleteInMemoryEventSubscription(self, subscription_name: str) -> None:
         self.object_manager.delete_from_memory_by_pk(
             EventSubscription.get_object_name(), EventSubscription.pk_column, subscription_name)
 
-    def describeEventSubscription(self, subscription_name: str, columns=['*']) -> EventSubscription:
-        event_subscription_dict = self.object_manager.get_from_memory(
-            EventSubscription.get_object_name(), columns, f'"{EventSubscription.pk_column} = "{subscription_name}"')
-        event_subscription = EventSubscription(**event_subscription_dict)
-        # for key, value in updates.items():
-        #     setattr(current_subscription, key, value)
+    def describeEventSubscription(self, subscription_name: str, columns: Optional[List[str]] = None, criteria: Optional[str] = None) -> EventSubscription:
+        event_subscription_dict = self.object_manager.get_all_objects_from_memory(
+            EventSubscription.get_object_name())
+        event_subscription = EventSubscription(
+            **json.loads(event_subscription_dict))
         return event_subscription
 
-    def modifyEventSubscription(self, subscription_name: str, updates: Dict[str, Any]):
-        self.object_manager.update_in_memory(object_name=self.table_name,
-                                             updates=updates, object_id=subscription_name)
+    def modifyEventSubscription(self, subscription_name: str, updates: Dict[str, Any]) -> None:
+        self.object_manager.update_in_memory(EventSubscription.get_object_name(),
+                                             updates, f'"{EventSubscription.pk_column}" = "{subscription_name}"')
