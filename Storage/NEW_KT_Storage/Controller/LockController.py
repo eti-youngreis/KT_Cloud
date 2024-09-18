578692ef21dbae69d982da09503fe1ad9b28e7cb
תמר מליק <tamar.m1225@gmail.com>
2024-09-18 16:25:34 +0300

    remove main

--- Storage/NEW_KT_Storage/Controller/LockController.py
+++ Storage/NEW_KT_Storage/Controller/LockController.py
@@ -24,28 +24,3 @@ class LockController:
     def can_delete_object(self, bucket_key: str, object_key: str):
         return self.service.can_delete_object(bucket_key, object_key)
     
-def main():
-    lock_service = LockService()
-    lock_controller = LockController(service=lock_service)
-
-    # Create a lock
-    bucket_key = "test3_bucket"
-    object_key = "today7bj"
-    lock_mode = "all"  
-    amount = 1
-    unit = "m"  
-    
-    lock_service.create_lock(bucket_key=bucket_key, object_key=object_key, lock_mode=lock_mode, amount=amount, unit=unit)
-    # lock_service.lock_manager.object_manager.object_manager.get_from_memory("Lock", criteria="LockId == 'test3_bucket.obj6'")
-    
-    # lock_controller.delete_lock("test3_bucket.obj6")
-    
-    # Keep the main thread alive
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Main thread interrupted.")
-
-main()
-
