9ba7e05f53c5922dbc9292a12ee8bb41c967b02c
תמר מליק <tamar.m1225@gmail.com>
2024-09-19 10:58:34 +0300

    deleteable => deletable

--- Storage/NEW_KT_Storage/Controller/LockController.py
+++ Storage/NEW_KT_Storage/Controller/LockController.py
@@ -21,7 +21,31 @@ class LockController:
     def is_object_updatable(self, bucket_key: str, object_key: str):
         return self.service.is_object_updatable(bucket_key, object_key)
     
-    def is_object_deleteable(self, bucket_key: str, object_key: str):
+    def is_object_deletable(self, bucket_key: str, object_key: str):
         return self.service.is_object_deletable(bucket_key, object_key)
     
+def main():
+    lock_service = LockService()
+    lock_controller = LockController(service=lock_service)
+
+    # Create a lock
+    bucket_key = "test3_bucket"
+    object_key = "todaybj"
+    lock_mode = "all"  
+    amount = 1
+    unit = "m"  
+    
+    lock_controller.create_lock(bucket_key=bucket_key, object_key=object_key, lock_mode=lock_mode, amount=amount, unit=unit)
+    # lock_service.lock_manager.object_manager.object_manager.get_from_memory("Lock", criteria="LockId == 'test3_bucket.obj6'")
+    
+    # lock_controller.delete_lock("test3_bucket.obj6")
+    
+    # Keep the main thread alive
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Main thread interrupted.")
+
+main()
 
