ef1f1bf3811393264525b1f49169e71887421eaf
shana-git <shana3229317@gmail.com>
2024-09-22 01:46:44 +0300

    small change

--- Storage/NEW_KT_Storage/Controller/MultipartUploadController.py
+++ Storage/NEW_KT_Storage/Controller/MultipartUploadController.py
@@ -22,13 +22,55 @@ class MultipartUploadController:
         self.service.upload_file_parts(upload_id, file_path, part_size)
 
     def list_parts(self, upload_id: str):
-        # Retrieves the list of parts associated with the given upload_id.
+        """
+        Retrieves the list of parts associated with the given upload_id.
+        """
         return self.service.list_parts(upload_id)
+    
+    def abort_multipart_upload(self, upload_id: str):
+        """
+        Retrieves the list of parts associated with the given upload_id.
+        """
+        return self.service.abort_multipart_upload(upload_id)
     
     def complete_upload(self, upload_id: str, bucket_name: str = None, object_key: str = None) -> str:
         """
         Completes the upload process and merges all parts into a single file.
         """
         return self.service.complete_upload(upload_id, bucket_name, object_key)
+    
+if __name__ == "__main__":
+    db_file = "my_db.db"
+    storage_path = "C:/Users/shana/Desktop/a"
+    
+    controller = MultipartUploadController(db_file, storage_path)
+    
+    # Initiates the upload
+    bucket_name = "my_bucket"
+    object_key = "my_file_shana-11.txt"
+    upload_id = controller.initiate_upload(bucket_name, object_key)
+    print(f"Upload ID: {upload_id}")
+    
+    # # # # Uploads the file parts
+    # # # controller.service.upload_part(upload_id, 1, "aaaa")
+    # # # controller.service.upload_part(upload_id, 2, "bbbb")
+    file_path = "C:/Users/shana/Desktop/a/my_file.txt"
+    controller.upload_file_parts(upload_id, file_path)
+    # print("File parts uploaded.")
+
+    # list_parts = controller.list_parts(upload_id)
+    # print(list_parts, "list_parts")
+
+    # controller.abort_multipart_upload('c3af8ddb-23fc-432a-a487-c54b66e4b3c6')
+    # print(f"abort_multipart_upload")
+
+
+    # # Completes the upload
+    complete_file_path = controller.complete_upload(upload_id, bucket_name, object_key)
+    print(f"Upload complete! File saved at: {complete_file_path}")
+
+    # Displays all existing multipart uploads
+    all_uploads = controller.service.select_all_from_table(db_file, "mng_MultipartUploads")
+    print("All multipart uploads:", all_uploads)
     
 
--- Storage/NEW_KT_Storage/DataAccess/MultipartUploadManager.py
+++ Storage/NEW_KT_Storage/DataAccess/MultipartUploadManager.py
@@ -23,31 +23,38 @@ class MultipartUploadManager:
 
 
     def create_multipart_upload(self, multipart_upload: MultipartUploadModel) -> str:
-        """Creates a multipart upload process and returns a unique UploadId."""
+        # Creates a multipart upload process and returns a unique UploadId.
         if not isinstance(multipart_upload, MultipartUploadModel):
             raise TypeError('Expected an instance of MultipartUploadModel')
+        # Saves the multipart upload record in memory (database)
         self.object_manager.save_in_memory(self.object_name, multipart_upload.to_sql())
         return multipart_upload.upload_id
 
-    def upload_part(self, multipart_upload: MultipartUploadModel, new_part: PartModel, body: str) -> str:
+    def upload_part(self, multipart_upload: MultipartUploadModel, new_part: PartModel) -> str:
+        # Adds a new part to an existing multipart upload.
+        # Updates the multipart upload record by appending the new part information.
         if isinstance(multipart_upload.parts, str):
             multipart_upload.parts = json.loads(multipart_upload.parts)
 
-        # Updates the upload object with the new part
+        # Append the new part's details (part number and file path) to the list of parts
         multipart_upload.parts.append({
             'PartNumber': new_part.part_number,
             'FilePath': new_part.part_file_path
         })
-        # # Update in the database
+
+        # Convert the list of parts to JSON format and update it in the database.
         str_parts = json.dumps(multipart_upload.parts)
         update_statement = f"parts = '{str_parts}'"
         criteria = f"object_id = '{multipart_upload.upload_id}'"
+        # Update the multipart upload record in memory with the new parts data.
         self.object_manager.update_in_memory(self.object_name, updates=update_statement, criteria=criteria)
-        # # Generate a fake ETag for the example
+        # Generate a fake ETag for the example
         return f'etag_{new_part.part_number}'
 
-    def complete_multipart_upload(self, multipart_upload: MultipartUploadModel):
-        # Deletes the multipart upload record from memory upon completion.
+
+    def delete_multipart_upload(self, multipart_upload: MultipartUploadModel):
+        # Deletes the multipart upload record from the database after the process is completed.
+        # Removes the record identified by its primary key (pk) from the database.
         self.object_manager.delete_from_memory_by_pk(self.object_name, multipart_upload.pk_column, multipart_upload.pk_value)
 
 
