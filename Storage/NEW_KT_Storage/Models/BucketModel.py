bd97e95d5c8f4c10fab9219fb3d3d67292a7a34d
malkimarxon <m0548501844@gmail.com>
2024-09-22 23:14:52 +0300

    fix the order

--- Storage/NEW_KT_Storage/Models/BucketModel.py
+++ Storage/NEW_KT_Storage/Models/BucketModel.py
@@ -21,8 +21,8 @@ class Bucket:
         return ObjectManager.convert_object_attributes_to_dictionary(
             bucket_name=self.bucket_name,
             owner=self.owner,
-            create_at=self.create_at,
-            region=self.region
+            region=self.region,
+            create_at=self.create_at
         )
 
     def to_sql(self):
