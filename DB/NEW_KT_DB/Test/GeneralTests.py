6e5a46b39e0fa9c657b041bbae67d1b2fa1ee88e
tamar koledetzky <tamarkoledetzky26@gmail.com>
2024-09-22 09:42:52 +0300

    add_test

--- DB/NEW_KT_DB/Test/GeneralTests.py
+++ DB/NEW_KT_DB/Test/GeneralTests.py
@@ -1,10 +1,27 @@
+import json
+import os
+import sys
 import pytest
-from KT_STORAGE import StorageManager 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
+from Storage.NEW_KT_Storage.DataAccess.StorageManager import StorageManager
 
-
+@pytest.fixture
 def storage_manager():
-    """Fixture to create an instance of OptionGroup."""
-    return StorageManager()
+    """Fixture to create an instance of StorageManager."""
+    return StorageManager('test')
 
-def test_file_exists(file_name):
-    assert storage_manager.is_file_exist(file_name)
+def assert_file_exists(storage_manager, file_name):
+    assert storage_manager.is_file_exist(file_name), f"Expected file {file_name} was not created."
+
+# Generic function to delete a file
+def delete_file_if_exists(storage_manager, file_name):
+    storage_manager.delete_file(file_name)
+
+# Generic function to load JSON file and assert its content
+def assert_json_content(storage_manager, file_name, expected_data):
+    full_path = os.path.join(storage_manager.base_directory, file_name)
+    with open(full_path, 'r') as json_file:
+        data = json.load(json_file)
+        for key, value in expected_data.items():
+            print(value)
+            assert data[key] == value, f"Expected {key} to be {value}, but got {data[key]}"
