833b502836890d39606b665d59ca34944541e459
saraNoigershel <s3002005505@gmail.com>
2024-09-22 13:09:54 +0300

    finish implementation

---
+++ DB/NEW_KT_DB/Exceptions/DBProxyEndpointExceptions.py
@@ -0,0 +1,32 @@
+from DB.NEW_KT_DB.Exceptions.GeneralExeptions import *
+from DB.NEW_KT_DB.Models.DBProxyEndpointModel import DBProxyEndpoint
+
+class DBProxyNotFoundException(ObjectNotFoundException):
+    def __init__(self, endpoint_name: str):
+        super().__init__(DBProxyEndpoint.object_name, endpoint_name)
+
+class DBProxyEndpointNotFoundException(ObjectNotFoundException):
+    def __init__(self, endpoint_name: str):
+        super().__init__(DBProxyEndpoint.object_name, endpoint_name)
+
+class DBProxyNotFoundException(ObjectNotFoundException):
+    def __init__(self, db_proxy_name: str):
+        super().__init__(DBProxyEndpoint.object_name, db_proxy_name)
+
+class DBProxyEndpointAlreadyExistsException(ObjectAlreadyExistsException):
+    def __init__(self, endpoint_name: str):
+        super().__init__(DBProxyEndpoint.object_name, endpoint_name)
+
+class DBProxyEndpointQuotaExceededException(Exception):
+    pass
+
+class InvalidDBProxyStateException(InvalidObjectStateException):
+    def __init__(self, endpoint_name: str, object_state:str):
+        super().__init__(DBProxyEndpoint.object_name, endpoint_name, 'available', object_state)
+
+class InvalidDBProxyEndpointStateException(InvalidObjectStateException):
+    def __init__(self, endpoint_name: str, object_state:str):
+        super().__init__(DBProxyEndpoint.object_name, endpoint_name, 'available', object_state)
+
+
+
---
+++ DB/NEW_KT_DB/Models/DBProxyEndpointModel.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from typing import Dict, List, Optional
+from DB.NEW_KT_DB.DataAccess.ObjectManager import ObjectManager
+import json
+
+class DBProxyEndpoint:
+    pk_column = 'DBProxyEndpointName'
+    object_name = 'DBProxyEndpoint'
+    foreign_table_name = 'db_proxies' # will change by DBProxy.object_name
+    table_structure = f"""
+    DBProxyEndpointName VARCHAR(63) PRIMARY KEY NOT NULL,
+    DBProxyName VARCHAR(63) NOT NULL,
+    TargetRole VARCHAR(10) NOT NULL,
+    Tags JSONB DEFAULT '{{}}',
+    Status VARCHAR(20) NOT NULL,
+    CreatedDate TIMESTAMP NOT NULL,
+    Endpoint TEXT NOT NULL,
+    IsDefault BOOLEAN NOT NULL,
+    FOREIGN KEY (DBProxyName) REFERENCES {foreign_table_name}(DBProxyName)"""
+
+    def __init__(self,  
+                 DBProxyEndpointName:str,
+                 DBProxyName:str, 
+                 TargetRole:Optional[str] = None, 
+                 Tags:Optional[List[Dict[str, str]]] = None, 
+                 Status:str = 'creating',
+                 created_date = datetime.now(),
+                 endpoint:str ='',
+                 IsDefault:bool = False): 
+        self.DBProxyName=DBProxyName
+        self.DBProxyEndpointName=DBProxyEndpointName
+        self.TargetRole=TargetRole
+        self.Tags = Tags
+        self.Status = Status
+        self.CreatedDate = created_date
+        self.Endpoint = endpoint if endpoint else DBProxyEndpointName+'.rds.amazonaws.com'
+        self.IsDefault = IsDefault
+        self.Status = 'available'
+
+       
+
+
+    def to_dict(self) -> Dict:
+        '''Retrieve the data of the DB cluster as a dictionary.'''
+
+        return ObjectManager.convert_object_attributes_to_dictionary(
+            DBProxyEndpointName = self.DBProxyEndpointName,
+            DBProxyName = self.DBProxyName,
+            TargetRole = self.TargetRole,
+            Tags = self.Tags,
+            Status = self.Status,
+            CreateDate = self.CreatedDate,
+            Endpoint = self.Endpoint,
+            IsDefault = self.IsDefault
+            
+        )
+    
+    
