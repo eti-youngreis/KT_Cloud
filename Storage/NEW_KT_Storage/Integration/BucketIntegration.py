8bb2419a26fa36b700f598708dc240bd2b56be33
malkimarxon <m0548501844@gmail.com>
2024-09-22 14:00:57 +0300

    Changes in prints

--- Storage/NEW_KT_Storage/Integration/BucketIntegration.py
+++ Storage/NEW_KT_Storage/Integration/BucketIntegration.py
@@ -6,10 +6,10 @@ os.chdir('D:/s3_project/KT_Cloud')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..','..')))
 from Storage.NEW_KT_Storage.Controller.BucketController import BucketController
 
-# demonstrate all object functionallity
+# demonstrate bucket functionallity
 print('''---------------------Start Of session----------------------''')
 start_time_session = datetime.now()
-print(f"{start_time_session} deonstration of object: Bucket start")
+print(f"{start_time_session} demonstration of object: Bucket start")
 
 start_time = datetime.now()
 bucket_controller = BucketController()
@@ -19,65 +19,56 @@ print()
 name_bucket = "example-bucket"
 owner = "Malki"
 region = "us-east-2"
-print(f"{datetime.now()} going to create bucket 'name: {name_bucket}, owner: {owner}, region: {region}'")
-bucket_controller.create_bucket(name_bucket,owner,region)
-print(f"{datetime.now()} bucket name: '{name_bucket}' created successfully")
+print(f"{datetime.now()} start creating bucket 'name: {name_bucket}, owner: {owner}, region: {region}'")
+bucket_controller.create_bucket(name_bucket,owner, region)
+print(f"{datetime.now()} bucket: '{name_bucket}' created successfully")
 end_time = datetime.now()
 total_duration = end_time - start_time
-print(f"the total duration of create bucket is: '{total_duration}'")
+print(f"total duration of create bucket is: '{total_duration}'")
 
 # test create bucket
 print()
-start_time = datetime.now()
-pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/BucketTest.py::test_create_bucket_valid'])
-print(f"{datetime.now()} test create bucket successfully")
-end_time = datetime.now()
-total_duration = end_time - start_time
-print(f"the total duration of test bucket is: '{total_duration}'")
+test_name = 'test_create_bucket_valid'
+print(f"Running test: {test_name}:")
+pytest.main(['-q', f'Storage/NEW_KT_Storage/Test/BucketTest.py::{test_name}'])
+
 
 # get
 print()
 start_time = datetime.now()
-print(f"{start_time} going to get bucket '{name_bucket}'")
+print(f"{start_time} start getting bucket '{name_bucket}'")
 bucket_example = bucket_controller.get_bucket(name_bucket)
 print(f"{datetime.now()} verify bucket 'name: {bucket_example.bucket_name}, owner: {bucket_example.owner}, \
 region: {bucket_example.region}, created_at: {bucket_example.create_at}'")
 end_time = datetime.now()
 total_duration = end_time - start_time
-print(f"the total duration of get bucket is: '{total_duration}'")
+print(f"total duration of get bucket is: '{total_duration}'")
 
 # test get bucket
 print()
-start_time = datetime.now()
-pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/BucketTest.py::test_get_with_valid_bucket'])
-print(f"{datetime.now()} test get bucket successfully")
-end_time = datetime.now()
-total_duration = end_time - start_time
-print(f"the total duration of test bucket is: '{total_duration}'")
-
+test_name = 'test_get_with_valid_bucket'
+print(f"Running test: {test_name}:")
+pytest.main(['-q', f'Storage/NEW_KT_Storage/Test/BucketTest.py::{test_name}'])
 
 # delete
 print()
 start_time = datetime.now()
-print(f"{start_time} going to delete bucket '{name_bucket}'")
+print(f"{start_time} start deleting bucket '{name_bucket}'")
 bucket_controller.delete_bucket(name_bucket)
 print(f"{datetime.now()} verify bucket '{name_bucket}' deleted by checking if it exist")
 end_time = datetime.now()
 total_duration = end_time - start_time
-print(f"the total duration of delete bucket is: '{total_duration}'")
+print(f"total duration of delete bucket is: '{total_duration}'")
 
 # test delete bucket
 print()
-start_time = datetime.now()
-pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/BucketTest.py::test_delete_bucket_existing'])
-print(f"{datetime.now()} test delete bucket successfully")
-end_time = datetime.now()
-total_duration = end_time - start_time
-print(f"the total duration of test bucket is: '{total_duration}'")
+test_name = 'test_delete_bucket_existing'
+print(f"Running test: {test_name}:")
+pytest.main(['-q', f'Storage/NEW_KT_Storage/Test/BucketTest.py::{test_name}'])
 
 print()
 end_time_session = datetime.now()
-print(f"{end_time_session} deonstration of object Bucket ended successfully")
+print(f"{end_time_session} demonstration of object Bucket ended successfully")
 total_duration_session = end_time_session - start_time_session
 print(f"the total duration of session bucket is: '{total_duration_session}'")
 print('''---------------------End Of session----------------------''')
