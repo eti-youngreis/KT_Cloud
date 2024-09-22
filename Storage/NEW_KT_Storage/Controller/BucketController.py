c61907df988edd047464e12416d5075fccf9b537
malkimarxon <m0548501844@gmail.com>
2024-09-22 12:49:57 +0300

    write bucket tIntegration

--- Storage/NEW_KT_Storage/Controller/BucketController.py
+++ Storage/NEW_KT_Storage/Controller/BucketController.py
@@ -2,8 +2,8 @@ from Storage.NEW_KT_Storage.Service.Classes.BucketService import BucketService
 
 class BucketController:
 
-    def __init__(self, service: BucketService):
-        self.service = service
+    def __init__(self):
+        self.service = BucketService()
 
     def create_bucket(self, bucket_name: str, owner: str,region=None):
         self.service.create(bucket_name, owner,region)
--- Storage/NEW_KT_Storage/Integration/BucketIntegration.py
+++ Storage/NEW_KT_Storage/Integration/BucketIntegration.py
@@ -1,26 +1,76 @@
+from datetime import datetime
+import pytest
+import os
+import sys
+os.chdir('D:/s3_project/KT_Cloud')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..','..')))
+from Storage.NEW_KT_Storage.Controller.BucketController import BucketController
 
 # demonstrate all object functionallity
+print('''---------------------Start Of session----------------------''')
+start_time_session = datetime.now()
+print(f"{start_time_session} deonstration of object: Bucket start")
 
-print'''---------------------Start Of session----------------------'''
-print(current_date_time)
-
-print('''{current_date_time} deonstration of object XXX start''')
+start_time = datetime.now()
+bucket_controller = BucketController()
 
 # create
-print('''{current_date_time} going to create bucket names "example"''')
-bucketController.create('example')
-print('''{current_date_time} bucket "example" created successfully''')
-print(total_duration)
+print()
+name_bucket = "example-bucket"
+owner = "Malki"
+region = "us-east-2"
+print(f"{datetime.now()} going to create bucket 'name: {name_bucket}, owner: {owner}, region: {region}'")
+bucket_controller.create_bucket(name_bucket,owner,region)
+print(f"{datetime.now()} bucket name: '{name_bucket}' created successfully")
+end_time = datetime.now()
+total_duration = end_time - start_time
+print(f"the total duration of create bucket is: '{total_duration}'")
+
+
+# get
+print()
+start_time = datetime.now()
+print(f"{start_time} going to get bucket '{name_bucket}'")
+bucket_example = bucket_controller.get_bucket(name_bucket)
+print(f"{datetime.now()} verify bucket 'name: {bucket_example.bucket_name}, owner: {bucket_example.owner}, \
+region: {bucket_example.region}, created_at: {bucket_example.create_at}'")
+end_time = datetime.now()
+total_duration = end_time - start_time
+print(f"the total duration of get bucket is: '{total_duration}'")
+
 
 # delete
-print('''{current_date_time} going to delete bucket "example"''')
-bucketController.delete('example')
-print('{current_date_time} verify bucket "example" deleted by checking if it exist')
-bucketTest.verify_deletion('example')
-print('''{current_date_time} bucket "example" deleted successfully''')
-print(total_duration)
+print()
+start_time = datetime.now()
+print(f"{start_time} going to delete bucket '{name_bucket}'")
+bucket_controller.delete_bucket(name_bucket)
+print(f"{datetime.now()} verify bucket '{name_bucket}' deleted by checking if it exist")
+end_time = datetime.now()
+total_duration = end_time - start_time
+print(f"the total duration of delete bucket is: '{total_duration}'")
 
-print('''{current_date_time} deonstration of object XXX ended successfully''')
-print(current_date_time)
-print'''---------------------End Of session----------------------'''
-print(total_duration)
+# test delete bucket
+print()
+start_time = datetime.now()
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/BucketTest.py::test_delete_bucket_existing'])
+print(f"{datetime.now()} test delete bucket successfully")
+end_time = datetime.now()
+total_duration = end_time - start_time
+print(f"the total duration of test bucket is: '{total_duration}'")
+
+# test create bucket
+print()
+start_time = datetime.now()
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/BucketTest.py::test_create_bucket_valid'])
+print(f"{datetime.now()} test create bucket successfully")
+end_time = datetime.now()
+total_duration = end_time - start_time
+print(f"the total duration of test bucket is: '{total_duration}'")
+
+print()
+end_time_session = datetime.now()
+print(f"{end_time_session} deonstration of object Bucket ended successfully")
+total_duration_session = end_time_session - start_time_session
+print(f"the total duration of session bucket is: '{total_duration_session}'")
+print('''---------------------End Of session----------------------''')
+
