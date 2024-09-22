ea621cfa79543fab94dbce96dd020dceb779c976
shana-git <shana3229317@gmail.com>
2024-09-22 13:19:31 +0300

    romoved local path

--- Storage/NEW_KT_Storage/Integration/MultiPertUploadIntegration.py
+++ Storage/NEW_KT_Storage/Integration/MultiPertUploadIntegration.py
@@ -18,7 +18,61 @@ print(f'{start_time_session} deonstration of object multipart start')
 # create
 start_time= datetime.now()
 print(f"{start_time} going to create multipart for object name example")
-upload_id=multipartUploadController.initiate_upload('my_bucket','example3.txt')
+upload_id=multipartUploadController.initiate_upload('my_bucket','example-shana.txt')
+end_time=datetime.now()
+print(f"{end_time} multipart for object name example created successfully with upload_id {upload_id}")
+total_duration = end_time - start_time
+print(total_duration)
+
+#test for create
+print("running test for function")
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/test_multipart_upload.py::test_initiate_multipart_upload'])
+
+#upload_file_parts
+start_time= datetime.now()
+print(f"{start_time} going to upload file parts for object name example")
+multipartUploadController.upload_file_parts(upload_id,"C:/Users/shana/Desktop/a/my_file.txt")
+end_time=datetime.now()
+print(f"{end_time} file parts for object name example uploaded successfully")
+total_duration = end_time - start_time
+print(total_duration)
+
+#test for upload_file_parts
+print("running test for function")
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/test_multipart_upload.py::test_upload_file_parts'])
+
+#list_parts
+start_time= datetime.now()
+print(f"{start_time} going to list parts for object name example")
+list_parts=multipartUploadController.list_parts(upload_id)
+end_time=datetime.now()
+print(f"{end_time} list parts for object name example listed successfully {list_parts}")
+total_duration = end_time - start_time
+print(total_duration)
+
+#test for list_parts
+print("running test for function")
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/test_multipart_upload.py::test_list_parts_success'])
+
+# comlete_file_parts
+start_time= datetime.now()
+print(f"{start_time} going to complete file parts for object name example")
+multipartUploadController.complete_upload(upload_id)
+end_time=datetime.now()
+print(f"{end_time} file parts for object name example completed successfully")
+total_duration = end_time - start_time
+print(total_duration)
+
+#test for comlete_file_parts
+print("running test for function")
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/test_multipart_upload.py::test_complete_upload_missing_parts'])
+
+
+
+# create
+start_time= datetime.now()
+print(f"{start_time} going to create multipart for object name example")
+upload_id=multipartUploadController.initiate_upload('my_bucket','example-new.txt')
 end_time=datetime.now()
 print(f"{end_time} multipart for object name example created successfully with upload_id {upload_id}")
 total_duration = end_time - start_time
@@ -42,27 +96,22 @@ print(f"{end_time} list parts for object name example listed successfully {list_
 total_duration = end_time - start_time
 print(total_duration)
 
-# # comlete_file_parts
-# start_time= datetime.now()
-# print(f"{start_time} going to complete file parts for object name example")
-# multipartUploadController.complete_upload(upload_id)
-# end_time=datetime.now()
-# print(f"{end_time} file parts for object name example completed successfully")
-# total_duration = end_time - start_time
-# print(total_duration)
+#abort_file_parts
+start_time= datetime.now()
+print(f"{start_time} going to abort file parts for object name example")
+multipartUploadController.abort_multipart_upload(upload_id)
+end_time=datetime.now()
+print(f"{end_time} file parts for object name example aborted successfully")
+total_duration = end_time - start_time
+print(total_duration)
 
-# #abort_file_parts
-# start_time= datetime.now()
-# print(f"{start_time} going to abort file parts for object name example")
-# multipartUploadController.abort_multipart_upload(upload_id)
-# end_time=datetime.now()
-# print(f"{end_time} file parts for object name example aborted successfully")
-# total_duration = end_time - start_time
-# print(total_duration)
+#test for abort_file_parts
+print("running test for function")
+pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/test_multipart_upload.py::test_abort_multipart_upload_invalid_upload_id'])
 
-#tests
-print("running test for functions")
-pytest.main(['-v', 'Storage/NEW_KT_Storage/Test/test_multipart_upload.py'])
+
+
+
 
 end_time_session=datetime.now()
 print(f'{end_time_session} deonstration of object multipart upload ended successfully')
