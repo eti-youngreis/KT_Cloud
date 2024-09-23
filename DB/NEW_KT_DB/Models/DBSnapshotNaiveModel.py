78bf2b0df4f460d2217309f4c28ef4f875ab142d
leah-bellahsen-git <37326942190@mby.co.il>
2024-09-23 12:39:54 +0300

    first commit

---
+++ DB/NEW_KT_DB/Models/DBSnapshotNaiveModel.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import json
+from datetime import datetime
+from typing import Dict, Optional
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..","..")))
+from DB.NEW_KT_DB.DataAccess import ObjectManager
+from DB.NEW_KT_DB.Validation.DBSnapshotNaiveValidations import is_valid_db_instance_id, is_valid_db_snapshot_description, is_valid_progress, is_valid_date, is_valid_url_parameter
+
+class SnapshotNaive:
+    BASE_PATH = "db_snapshot"
+    object_name = "db_snapshot_naive"
+    pk_column = "db_snapshot_id"
+    pk_column_data_type = 'TEXT'
+    table_structure = f'''
+        db_instance_identifier TEXT PRIMARY KEY,
+        metadata TEXT NOT NULL
+        '''
+    def __init__(self, db_snapshot_identifier, db_instance_identifier: str, creation_date: datetime, owner_alias: str, status: str,
+                 description: Optional[str] = None, progress: Optional[str] = None, url_snapshot: Optional[str] = None):
+        
+        # Validate parameters
+        if not is_valid_db_instance_id(db_instance_identifier):
+            raise ValueError(f"Invalid db_instance_identifier: {db_instance_identifier}")
+        if not is_valid_date(creation_date.strftime('%Y-%m-%d')):
+            raise ValueError(f"Invalid creation_date: {creation_date}")
+        if description:
+            if not is_valid_db_snapshot_description(description):
+                raise ValueError(f"Invalid description: {description}")
+        if progress:
+            if not is_valid_progress(progress):
+                raise ValueError(f"Invalid progress: {progress}")
+        if url_snapshot:
+            if not is_valid_url_parameter(url_snapshot):
+                raise ValueError(f"Invalid url_snapshot: {url_snapshot}")
+        self.db_snapshot_identifier = db_snapshot_identifier
+        self.db_instance_identifier = db_instance_identifier
+        self.creation_date = creation_date
+        self.owner_alias = owner_alias
+        self.status = status
+        self.description = description
+        self.progress = progress
+        self.url_snapshot = url_snapshot
+        self.object_name = "Snapshot"
+        self.table_structure = {}
+
+    def to_dict(self) -> Dict:
+        '''Retrieve the data of the DB snapshot as a dictionary.'''
+        
+        return ObjectManager.convert_object_attributes_to_dictionary(
+            db_snapshot_identifier = self.db_snapshot_identifier,
+            db_instance_identifier = self.db_instance_identifier,
+            creation_date = self.creation_date,
+            owner_alias = self.owner_alias,
+            status = self.status,
+            description = self.description,
+            progress = self.progress,
+            url_snapshot = self.url_snapshot,
+            object_name = self.object_name,
+            table_structure = self.table_structure
+        )
+
+    def to_sql(self):
+        # Convert the model snapshot to a dictionary
+        data_dict = self.to_dict()
+        values = '(' + ", ".join(f'\'{json.dumps(v)}\'' if isinstance(v, dict) or isinstance(v, list) else f'\'{v}\'' if isinstance(v, str) else f'\'{str(v)}\''
+                                 for v in data_dict.values()) + ')'
+        return values
+
+
---
+++ DB/NEW_KT_DB/Validation/DBSnapshotNaiveValidations.py
@@ -0,0 +1,34 @@
+import re
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..","..")))
+from DB.NEW_KT_DB.Validation.GeneralValidations import is_length_in_range, is_valid_number, is_valid_db_instance_identifier
+from typing import Optional,Dict
+
+def is_valid_db_snapshot_description(description_snapshot: str) -> bool:
+    return is_length_in_range(description_snapshot, 1, 40)
+
+
+def is_valid_progress(progress: str) -> bool:
+    num_of_progress = progress[:-1]
+    num_of_progress_int = int(num_of_progress)
+    return is_valid_number(num_of_progress_int, 0, 100)
+
+
+def is_valid_date(date) -> bool:
+    pattern = r'^\d{4}-\d{2}-\d{2}$'  # Date format: YYYY-MM-DD
+    return bool(re.match(pattern, date))
+
+# db_instance_identifier
+
+def is_valid_db_instance_id(db_instance_identifier: str) -> bool:
+    return is_valid_db_instance_identifier(db_instance_identifier, 15)
+
+
+def is_valid_url_parameter(url_snapshot: str) -> bool:
+    '''Check if the url_snapshot parameter is valid.'''
+    # pattern = r'^[\w\-]+(?:%[0-9A-Fa-f]{2})*$'
+    # return bool(re.match(pattern, url_snapshot))
+    return True
+
+
